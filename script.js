402b3a8645077787c5c4780c35bc2f029e326ab5
Darwin Lyons <whoischarles@gmail.com>
2019-06-17 14:17:02 -0400

    nav

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+$(function () {
+
+  $('p.hamburger').on('click', function () {
+    $('nav').toggleClass('show');
+    $('.fa-times').toggleClass('show');
+    $('#toggle').toggle('slow');
+    console.log("what");
+  });
+
+  $(".fa-times").on("click", function () {
+    $('nav').toggleClass('show');
+    $('.fa-times').toggleClass('show');
+    
+    $('#toggle').toggle('slow');
+    
+  });
+});
